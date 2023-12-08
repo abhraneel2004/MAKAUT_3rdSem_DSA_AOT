b7988ff1dc63bb236adaac3be3cae16ea8ed2dcd
Abhraneel-11-06 <abhraneel.karmakar.22@aot.edu.in>
2023-12-09 01:31:25 +0530

    Added Stack implementation using Linked List

---
+++ 12_StackUsingLinkedList.cpp
@@ -0,0 +1,143 @@
+#include <iostream>
+using namespace std;
+
+// structure of a node in C++
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int value)
+    {
+        data = value;
+        next = nullptr;
+    }
+};
+
+// single linked list creation
+
+class StackLL
+{
+    Node *head;
+
+public:
+    StackLL()
+    {
+        head = NULL;
+    }
+    void push(int value)
+    {
+        Node *newNode = new Node(value);
+
+        if (!head)
+        {
+            head = newNode;
+        }
+
+        else
+        {
+            Node *temp = head;
+            while (temp->next)
+                temp = temp->next;
+            temp->next = newNode;
+        }
+    }
+
+    void pop()
+    {
+        if (!head)
+        {
+            cout << "Stack Underflow!" << endl;
+            return;
+        }
+        if (!head->next)
+        {
+            delete head;
+            head = NULL;
+            return;
+        }
+        Node *temp = head;
+        Node *prev = nullptr;
+        while (temp->next)
+        {
+            prev = temp;
+            temp = temp->next;
+        }
+        prev->next = nullptr;
+        delete temp;
+        return;
+    }
+
+    void peek()
+    {
+        if (!head)
+        {
+            return;
+        }
+        Node *temp = head;
+        while (temp->next)
+            temp = temp->next;
+        cout << temp->data << endl;
+    }
+
+    void display()
+    {
+        if (head == NULL)
+            return;
+        Node *temp = head;
+        while (temp)
+        {
+            cout << temp->data << "-> ";
+            temp = temp->next;
+        }
+        cout << "NULL \n";
+    }
+};
+
+int main(void)
+{
+    StackLL l;
+    int c;
+    cout << "Stack Operations\n";
+    do
+    {
+        cout << "-----------------------\n";
+        cout << "1. Push\n";
+        cout << "2. Pop\n";
+        cout << "3. Peek\n";
+        cout << "4. Display\n";
+        cout << "5. Exit\n";
+        cout << "-----------------------\n";
+        cout << "Enter Your choice: ";
+        cin >> c;
+        switch (c)
+        {
+        case 1:
+            int val;
+            cout << "Enter the data: ";
+            cin >> val;
+            l.push(val);
+            break;
+        case 2:
+            l.pop();
+            break;
+        case 3:
+            l.peek();
+            break;
+        case 4:
+            cout << "Displaying the Stack: ";
+            l.display();
+            cout << "\n";
+            break;
+        case 5:
+            break;
+        default:
+            cout << "Invalid choice\n";
+            break;
+        }
+    } while (c != 5);
+
+    return 0;
+}

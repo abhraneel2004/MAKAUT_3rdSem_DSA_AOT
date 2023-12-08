29818843607926ed9f28d2f049289194ca67dd42
Abhraneel-11-06 <abhraneel.karmakar.22@aot.edu.in>
2023-12-09 00:06:38 +0530

    Added insertion sort and modified quick sort

---
+++ 21_insertionSort.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+void InsertionSort(int* arr, int n){
+    for(int i = 1; i<n; i++){
+        int key = *(arr+i);
+        int j = i-1;
+
+        while (j>=0 && *(arr+j) > key){
+            *(arr+j+1) = *(arr+j);
+            j--;
+        }
+        *(arr+j+1) = key;
+
+    }
+}  
+
+int main(void){
+    int n,i;
+    cout<<"Enter array size: ";
+    cin >> n;
+    int arr[n];
+    cout<<"Enter array element: \n";
+    for( i = 0; i < n; i++){
+        cout<<"Enter element "<<i<<" : ";
+        cin>>arr[i];
+    }
+    cout<<"Array before sorting: ";
+    for( i = 0; i < n-1; i++){
+        cout<<arr[i]<<", ";
+    }
+    cout<<arr[i]<<endl;
+    InsertionSort(arr, n);
+    cout<<"Insertion Sort done\n";
+    cout<<"Array after sorting: ";
+    for(i = 0; i < n-1; i++){
+        cout<<arr[i]<<", ";
+    }
+    cout<<arr[i]<<endl;
+}

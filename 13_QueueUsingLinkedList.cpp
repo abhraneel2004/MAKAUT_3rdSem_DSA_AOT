68ae0a9129a02c797333c7c7f6ff8e5abc2a05a7
Abhraneel-11-06 <abhraneel.karmakar.22@aot.edu.in>
2023-12-09 01:36:59 +0530

    Queue using Singly linked list

---
+++ 13_QueueUsingLinkedList.cpp
@@ -0,0 +1,117 @@
+#include<iostream>
+using namespace std;
+
+
+// structure of a node in C++
+
+
+class Node{
+	public:
+		int data;
+		Node * next;
+		
+		Node(int value){
+			data = value;
+			next = nullptr;
+		}
+};
+
+
+
+// single linked list creation
+
+class QueueLL{
+	Node * head;
+	public:
+			QueueLL(){
+				head = NULL;
+			}
+			void insert(int value){
+				Node* newNode = new Node(value);
+
+                if(!head){
+                    head = newNode;
+                }
+                
+                else{
+                    Node * temp = head;
+                    while(temp->next) temp = temp->next;
+                    temp->next = newNode;
+                }
+                
+                
+			}
+
+            void pop(){
+                if(!head){
+                    cout<<"Queue Underflow!"<<endl;
+                    return;
+                }
+                else{
+                    Node * temp = head;
+                    head = head->next;
+                    delete temp;
+                    return;
+                }
+
+            }
+
+            void peek(){
+                if(!head){return;}
+                Node* temp = head;
+                while(temp->next) temp = temp->next;
+                cout<<temp->data<<endl;
+            }
+			
+			void display(){
+				if (head==NULL) return;
+				Node * temp = head;
+				while (temp){
+					cout<<temp-> data<<"-> ";
+					temp = temp-> next;
+				}
+                cout<<"NULL \n";
+			}
+};
+
+int main(void){
+	QueueLL l;
+	int c;
+	cout<<"Queue Operations\n";
+	do{
+		cout<<"-----------------------\n";
+		cout<<"1. Insert\n";
+        cout<<"2. Delete\n";
+		cout<<"3. Display\n";
+		cout<<"4. Exit\n";
+		cout<<"-----------------------\n";
+		cout<<"Enter Your choice: ";
+		cin>>c;
+		switch(c){
+			case 1:
+				int val;
+				cout<<"Enter the data: ";
+				cin>>val;
+				l.push(val);
+				break;
+            case 2:
+                l.pop();
+                break;
+
+            case 3:
+                cout<<"Displaying the Stack: ";
+				l.display();
+                cout<<"\n";
+                break;
+            case 4: break;
+			default:
+				cout<<"Invalid choice\n";
+				break;
+		}
+	}while (c!=4);
+	
+	
+	return 0;
+}
+
+

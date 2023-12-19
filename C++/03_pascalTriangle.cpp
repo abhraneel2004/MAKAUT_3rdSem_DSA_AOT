a73562d99372fbb1678bae016bdd99bdffe1b165
Abhraneel-11-06 <abhraneel.karmakar.22@aot.edu.in>
2023-12-19 23:26:34 +0530

    Files added

--- C++/03_pascalTriangle.cpp
+++ C++/03_pascalTriangle.cpp
@@ -1,12 +1,14 @@
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 
 int main(void){
 	int n, coeff;
 	printf("Enter no. of rows: ");
 	scanf("%d",&n);
 	for (int i =0; i<n; i++){
+		if(i%2==1)printf("\t");
 		for (int k =0; k<((n-i)/2); k++)
-			printf("\t");
+			printf("\t\t");
 		for (int j = 0; j<=i; j++){
 			if (i==0|| j==0){
 				coeff=1;

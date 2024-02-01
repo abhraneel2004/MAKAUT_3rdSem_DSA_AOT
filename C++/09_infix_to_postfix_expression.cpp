d86241d87f4c8ec8cde30c230ec636db8a83b2b8
abhraneel2004 <contact.abhraneel@gmail.com>
2024-02-01 23:38:35 +0530

    File Modified

--- C++/09_infix_to_postfix_expression.cpp
+++ C++/09_infix_to_postfix_expression.cpp
@@ -1,17 +1,44 @@
-#include <iostream>
+#include<iostream>
+using namespace std;
 #define MAX 100
 #include "Stack1.cpp"
 
 char infix[MAX], postfix[MAX];
-Stack1 st;
 
-
-int isAlNum(char x){
-	return ((x >= 'a' && x <= 'z') || (x >= 'A' && x <= 'Z') || (x >= '0' && x <= '9'));
+int balanced(char arr[]){
+    int i = 0;
+    Stack1 s1;
+    while(arr[i]!='\0'){
+        char c = arr[i];
+        if (c=='(' || c=='{' || c=='[')
+            s1.push(c);
+        else if (c==')'){
+            if (s1.isEmpty()|| s1.peek()!='('){
+                // cout<<"Invalid Expression\n";
+                return 0;
+            }
+            s1.pop();
+        }
+        else if (c=='}'){
+            if (s1.isEmpty()|| s1.peek()!='{'){
+                // cout<<"Invalid Expression\n";
+                return 0;
+            }
+            s1.pop();
+        }
+        else if (c==']'){
+            if (s1.isEmpty()|| s1.peek()!='['){
+                // cout<<"Invalid Expression\n";
+                return 0;
+            }
+            s1.pop();
+        }
+        i++;
+    }
+    return s1.isEmpty();
 }
-
 int prec(char x){
-	switch (x)
+    switch (x)
 	{
 	case '^':
 		return 3;
@@ -31,62 +58,55 @@ int prec(char x){
 	}
 }
 
-
-void in_post(char in[]){
-	int i = 0, p = 0;
-    char next;
-    while (in[i] != '\0')
-    {
-        char c = in[i];
-        if (c == '(')
-        {
-            st.push(c);
+void in_to_post(char arr[]){
+    int i = 0, p = 0;
+    char c,next;
+    Stack1 opStack;
+    while(arr[i]!='\0'){
+        c = arr[i];
+        if (c == '('){
+            opStack.push(c);
         }
-        else if (c == ')')
-        {
-            char next;
-            while ((next = st.pop()) != '(')
-            {
-                postfix[p++] = next;
+        else if(c==')'){
+            while((next = opStack.pop())!='('){
+                postfix[p++]=next;
             }
         }
-        else if (c == '+' || c == '-' || c == '*' || c == '/' || c == '^' || c == '%')
-        {
-            while (st.top != -1 && prec(c) <= prec(st.peek()))
-            {
-                postfix[p++] = st.pop();
+        else if(c=='+' || c=='-' || c== '*' || c=='/'){
+            while(opStack.top != -1 && prec(c)<=prec(opStack.peek())){
+                postfix[p++] = opStack.pop();
             }
-            st.push(c);
+            opStack.push(c);
         }
-        else
-        {
+        else{
             postfix[p++] = c;
         }
         i++;
     }
-    while (st.top != -1)
-    {
-        postfix[p++] = st.pop();
+    while(opStack.top != -1){
+        postfix[p++] = opStack.pop();
     }
     postfix[p] = '\0';
 }
 
 int main(void){
-	cout<<"Enter the Infix expression: ";
-	cin>>infix;
-	in_post(infix);
-	for (int i  = 0; i<(sizeof(postfix)/sizeof(char));i++){
-		if (postfix[i] == '('){
-			cout<<"Invalid Expression\n";
-			return 0;
-		}
-	}
-	cout<<"The postfix expression is: "<<postfix<<endl;
-	return 0;
-	
+    cout<<"Enter the infix expression: ";
+    cin>>infix;
+    if(balanced(infix)){
+        in_to_post(infix);
+        for(int i = 0; i< (sizeof(postfix)/sizeof(char));i++){
+            if (postfix[i]=='('){
+                cout<<"Invalid Expression\n";
+                return 1;
+            }
+        }
+        cout<<"Equivalent Postfix Expression is: "<<postfix<<endl;
+    }
+    else{
+        cout<<"Invalid Expression\n";
+    }
+    
+    return 0;
 }
-
-
-
 
 

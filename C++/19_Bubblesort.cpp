292c38c2b109b708b30e0d03c7abe720569bca5b
abhraneel2004 <contact.abhraneel@gmail.com>
2024-02-16 23:05:26 +0530

    Files Modified

--- C++/19_Bubblesort.cpp
+++ C++/19_Bubblesort.cpp
@@ -20,23 +20,28 @@ int main(void){
         cout<<arr[i]<<"\t";
     
     cout<<"\n\n~~~~~~~~~Bubble Sort~~~~~~~~~\n\n";
-
+    int swap1 = 0;
     for (int i = 0; i<n-1; i++){
         c = 0;
         for (int j = 0; j<n-i-1; j++){
             if (arr[j]>arr[j+1]){
                 c++;
                 swap(arr, j, j+1);
+                swap1++;
             }
         }
         if (!c) break;
+        cout<<"Pass "<<i+1<<": \n";
+        for (int i = 0; i<n ; i++)
+            cout<<arr[i]<<"\t";
+        cout<<endl;
     }
     
     cout<<"After Sorting the array is :\n";
     for (int i = 0; i<n ; i++)
         cout<<arr[i]<<"\t";
     cout<<"\n";
-    
+    cout<<"No of swaps: "<<swap1<<endl;
 
     return 0;
 }
